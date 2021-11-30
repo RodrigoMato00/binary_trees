d6fb7acaed6f49c3dcea52512398e4ac602018c5
RodrigoMato00 <3293@holbertonschool.com>
2021-11-30 13:11:59 -0800

    13 funcional

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,6 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left || tree->right)
 		return (1 + binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right));
+
 	else
 		return (0);
 }
