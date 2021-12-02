da0074c7553f02b08aae3b619d391f07fed3cd85
RodrigoMato00 <3293@holbertonschool.com>
2021-12-01 17:27:24 -0800

    101 funcionando

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_complete - function that checks if a binary tree is complete
+ *@tree: pointer to the root node of the tree to check
+ *Return: 0 or 1
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left == NULL) && (tree->right))
+		return(0);
+
+	if ((tree->right == NULL) && (tree->left == NULL))
+		return (0);
+
+	else
+		return (1);
+}
